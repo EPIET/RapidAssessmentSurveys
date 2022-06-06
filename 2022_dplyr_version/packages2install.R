6ceb4e28973a6c7a7970cb06ee9d9196207cf192
Amy Mikhail <amy.mikhail@gmail.com>
2022-06-06 07:28:17 +0100

    Added complete task 5.2

--- 2022_dplyr_version/packages2install.R
+++ 2022_dplyr_version/packages2install.R
@@ -5,20 +5,30 @@
 # This script uses the p_load() function from pacman R package, 
 # which installs if package is absent, and loads for use if already installed
 
+# Packages to install *before* running pacman pload():
+#####################################################
 
 # Ensures the package "pacman" is installed
-if (!require("pacman")) install.packages("pacman")
+if (!require("pacman")) install.packages("pacman", quiet = TRUE)
+
+# Ensure the package "remotes" is installed
+if (!require("remotes")) install.packages("remotes", quiet = TRUE)
+
+# Install the epikit package from Github:
+if (!require("epikit")) remotes::install_github("R4EPI/epikit", quiet = TRUE)
+
+# Install the sitrep package from Github:
+if (!require("sitrep")) remotes::install_github("R4EPI/sitrep", quiet = TRUE)
 
 
-# Packages available from CRAN
-##############################
+# Install and/or load required packages:
+########################################
 pacman::p_load(
   
   # Project and file management
   #############################
   here,      # file paths relative to R project root folder
   rio,       # import/export of many types of data
-  openxlsx,  # import/export of multi-sheet Excel workbooks
   rmarkdown, # reading and printing RAS R markdown guide
   knitr,     # reading and printing RAS R markdown guide
   
@@ -38,46 +48,34 @@ pacman::p_load(
   #lubridate,  # work with dates
   #purrr       # iteration and working with lists
   janitor,     # data cleaning
-  plotly,      # Interactively inspecting exploratory graphs
-  
-  # Exploratory graphs:
-  #########################
-  metR,         # Filled contours used with ggplot2
-  gridExtra,	 # Plotting ggplot2 graphs side by side
- 
   
   # Summary tables:
   #########################
   flextable,   # Creating printable summary tables
   officer,     # Fine tuning flextables
-  DT, 			   # Interactive tables
-  broom,       # Extracting summary information from models
   gtsummary,   # making descriptive and statistical tables
-  rstatix,     # quickly run statistical tests and summaries
+  smd,         # Helper functions for gtsummary tables
+  labelled,    # Labelling variables and values
   scales,      # helper functions
   
   # RAS analysis:
   #########################
-  # Package functions will be explained in the guide
-  srvyr,
-  epikit,
-  Hmisc,
-  survey,
-  gmodels,
-  epiR, 
-  multcomp,
-  leaps,
-  car,
-  lme4,
-  lmerTest,
-  performance,
-  optimx,
-  randomNames
-
+  pps,         # Probability proportional to size (PPS) sampling
+  sampler,     # Calculate sample size
+  srvyr,       # Create survey design and calculate vaccination coverage
+  survey,      # Support for srvyr functions
+  glmmML,      # Poisson and regression mixed effects models with clustering
+  Hmisc        # Summary functions
 )
 
+# Check for and install Github packages:
+pacman::p_load_gh(
+  
+  # Packages for survey analysis and mixed effects:
+  #################################################
+  "R4EPI/epikit",      # Functions for survey analysis
+  "R4EPI/sitrep"      # Function wrappers for survey analysis
+)
 
-# Additional package for mixed effects modelling:
-remotes::install_github("goodekat/redres")
 
 #####################################################
